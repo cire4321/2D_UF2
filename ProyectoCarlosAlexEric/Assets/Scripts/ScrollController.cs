7620e32ba9a8415a1cdfe93c08d58b24640ec4c5
carlosasin14 <59094230+carlosasin14@users.noreply.github.com>
2020-02-10 09:55:54 +0100

    MiParte

---
+++ ProyectoCarlosAlexEric/Assets/Scripts/ScrollController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollController : MonoBehaviour
+{
+    public float speed;
+
+    private MeshRenderer rnd;
+    private Material mat;
+    private Vector2 offset;
+    
+    void Start()
+    {
+        rnd = GetComponent<MeshRenderer>();
+        mat = rnd.material;
+        offset = mat.mainTextureOffset;
+    }
+
+    void Update()
+    {
+        offset.x += speed * Time.deltaTime;
+
+        mat.mainTextureOffset = offset;
+    }
+}

bf8c55d509b8576f46a0dde24f80e0691aec26ed
carlosasin14 <59094230+carlosasin14@users.noreply.github.com>
2020-02-19 13:05:27 +0100

    19/02/2020

---
+++ ProyectoCarlosAlexEric/Assets/Scripts/Level/DefeatMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DefeatMenu : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene("Candyland");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
